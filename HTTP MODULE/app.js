e90293749f26c41a8554bf229c61949bbc67a77c
bharath rao <bharathraon@outlook.com>
2022-05-23 17:23:33 +0530

    used just res.end() instead of using both end() and write()

--- HTTP MODULE/app.js
+++ HTTP MODULE/app.js
@@ -3,12 +3,9 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   // this is used to know the endpoint/adress the client is requesting
   if (req.url == "/") {
-    // this is used to send user data on the webpage
-    res.write("welcome to our homepage");
+    // We could however send our response through end() without using write()
+    res.end("Welcome to our homepage");
   }
-
-  // we have end the server, after serving so the security isn't compromised
-  res.end();
 });
 
 server.listen(5000);
