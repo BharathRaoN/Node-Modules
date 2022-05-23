2a8f43188871dde75110c11780de2c5c731c57e9
bharath rao <bharathraon@outlook.com>
2022-05-23 22:36:42 +0530

    used fs module to get the content from a file, and then used those read content to create another file

---
+++ FS MODULE/fs-sync.js
@@ -0,0 +1,16 @@
+// destructuring and getting only readFileSync and writeFileSync from the fs module
+const { readFileSync, writeFileSync } = require("fs");
+
+// reading the content in first.txt using readFileSync method same goes for second.txt
+let first = readFileSync("../CONTENT/first.txt", "utf-8");
+let second = readFileSync("../CONTENT/second.txt", "utf-8");
+
+/* on writeFileSync even if we give a non existent file it will create a file in the given path and
+writes the content we have given as the second arguement
+*/
+writeFileSync(
+  "../CONTENT/result.txt",
+  `
+here is the result text file : ${first}  ${second}
+`
+);
