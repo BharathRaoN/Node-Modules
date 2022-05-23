c6402755ebb128942f857eb03e6516cbcbf5fe79
bharath rao <bharathraon@outlook.com>
2022-05-23 17:35:51 +0530

    some more choices and a default error page is added

--- HTTP MODULE/app.js
+++ HTTP MODULE/app.js
@@ -6,6 +6,20 @@ const server = http.createServer((req, res) => {
     // We could however send our response through end() without using write()
     res.end("Welcome to our homepage");
   }
+
+  if (req.url == "/about") {
+    res.end("Here is our short history");
+  }
+
+  //this is the default if the requesting content doesn't exist
+  //normal response
+  res.end("sorry this page doesn't exist");
+  //html response: Note:use object literal for multiline comment
+  res.end(`
+  <h1>oops!!</h1>
+  <p>this page doesn't exist</p>
+  <a href="/">go to homepage</a>
+  `);
 });
 
 server.listen(5000);
